2cfc2450962eb7138620a1be0bc2f9c2626de67b
AnnaGD <anna.gonch.liaison@gmail.com>
2025-05-04 14:58:47 -0500

    Set up m,ain for auto generating docs

--- backend/go.mod
+++ backend/go.mod
@@ -3,3 +3,5 @@ module obolus/backend
 go 1.23.5
 
 require github.com/go-chi/chi/v5 v5.2.1
+
+require github.com/go-chi/docgen v1.3.0 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,39 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"obolus/backend/routes"
+	"github.com/go-chi/docgen"
 )
 
-// Controller
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(
-		w, `
-          ##         .
-    ## ## ##        ==
- ## ## ## ## ##    ===
-/"""""""""""""""""\___/ ===
-{                       /  ===-
-\______ O           __/
- \    \         __/
-  \____\_______/
-
-	
-Hello from Docker!
-
-`,
-	)
-}
+// passing the routes flag to print docs -- to run it: `go run . -routes`
+var printRoutes = flag.Bool("routes", false, "Generate router documentation")
 
 func main() {
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
-	router.Get("/", handler)
+	flag.Parse() // see if the route was passed to generate documentation
+
+	router := routes.InitRoutes() // create a new router
 
 	fmt.Println("Go backend started!")
+
+	// Passing -routes to the program will generate docs for the above router definition.
+	// See the `routes.json` file in this folder for the output.
+
+	if *printRoutes {
+		fmt.Println(docgen.MarkdownRoutesDoc(router, docgen.MarkdownOpts{
+			ProjectPath: "https://github.com/AnnaGD/obolus",
+			Intro: "Welcome to Obolus, your fave travel coordinator!",
+		}))
+		return
+	}
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
