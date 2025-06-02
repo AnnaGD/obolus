a464a854e0287d17a6d7a539d642a4e67383af09
AnnaGD <anna.gonch.liaison@gmail.com>
2025-06-02 18:40:45 -0500

    Set up CORS middleware

--- backend/go.mod
+++ backend/go.mod
@@ -10,3 +10,5 @@ require (
 	github.com/mattn/go-sqlite3 v1.14.28
 	golang.org/x/crypto v0.38.0
 )
+
+require github.com/go-chi/cors v1.2.1 // indirect
--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -8,15 +8,30 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 // Initialize ther route mapping
 func InitRoutes(db *sql.DB) *chi.Mux{
 	router := chi.NewRouter()
+
+	// Set up CPRS middleware options
+	corsMiddleware := cors.New(cors.Options{
+		// AllowedOrigins can be set to the origins that you expect to make requests from, or to "*" to allow any origin
+		AllowedOrigins: []string{"*"}, // Adjust the port and domain as needed "http://localhost:3000", "https://yourdomain.com"
+
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"}, // "Accept", "Authorization", "Content-Type", "X-CSRF-Token"
+
+		ExposedHeaders: []string{},
+		AllowCredentials: true,
+		MaxAge: 200, // Maximum value not to be exceeded for Access-Control-Max-Age in seconds
+
+	})
+
+	router.Use(corsMiddleware.Handler)
 	router.Use(middleware.Logger)
 	router.Use(middleware.Recoverer)
-
-	router.Get("/", controllers.HomeHandler)
 
 	// User routes
 	router.Post("/signup", controllers.SignUpHandler(db))
